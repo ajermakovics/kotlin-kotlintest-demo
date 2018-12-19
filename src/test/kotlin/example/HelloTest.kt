55e3c2778ba48ccad4c4a00f9a8e4bd13790ef46
ajermakovics <ajermakovics@users.noreply.github.com>
2018-12-19 15:30:16 +0000

    Add test output

--- src/test/kotlin/example/HelloTest.kt
+++ src/test/kotlin/example/HelloTest.kt
@@ -6,6 +6,8 @@ import io.kotlintest.specs.StringSpec
 class HelloTest : StringSpec({
 
     "length should return the size of string" {
+        println("Running test")
+        RuntimeException().printStackTrace()
         hello("KotlinTest") shouldBe "Hello, KotlinTest!"
     }
 
